52b007eb4b99a9912f48f4a2e15fe22863183547
Mark Weng <mark.weng@tw.abb.com>
2022-05-02 18:41:33 +0800

    Correct StatefulWidget implement

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
     print(questionIndex);
   }
 
@@ -19,7 +30,6 @@ class MyApp extends StatelessWidget {
     'animals',
   ];
 
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
